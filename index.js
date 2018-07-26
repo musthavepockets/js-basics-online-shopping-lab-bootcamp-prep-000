b1c8f33e24d20c52f08a22ed2f952336420b6564
cd <coraldarling@outlook.com>
2018-07-26 19:37:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ function removeFromCart(item) {
   for (var i = 0; i <= cart.length; i++) {
   if (item === `${cart[i].itemName}`) {
       cart.splice([i], 1);
-  return cart;
+      getCart();
   } else { 
     return ('That item is not in your cart.');
 }
