8b3209ddcd1256dd67c48860788e7961ca828c26
cd <coraldarling@outlook.com>
2018-07-26 16:34:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -38,8 +38,8 @@ function viewCart() {
 function total() {
   for (var i = 0; i < cart.length; i++) {
     var cartTotal = []
-    cartTotal += cart[i].itemPrice;
-    return cartTotal;
+    cartTotal.push(cart[i].itemPrice);
+    return cartTotal.sum;
 }
 }
 
