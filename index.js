893af8f66c17fc2cc5f29c6d9a89cf94a10c9762
cd <coraldarling@outlook.com>
2018-07-26 19:57:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ function total() {
 }
 
 function removeFromCart(item) {
-  for (var i = 0; i <= cart.length; i++) {
+  for (let i = 0; i <= cart.length; i++) {
     if (item === `${(cart[i].itemName)}`) {
       cart.splice([i], 1);
       getCart();
