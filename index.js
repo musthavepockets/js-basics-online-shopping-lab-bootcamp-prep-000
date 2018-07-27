847c09587b269cb46a0aaff8cbe15aba57bd85be
cd <coraldarling@outlook.com>
2018-07-27 00:27:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ function placeOrder(cardNumber) {
     if (cardNumber > 0) {
     for (let i = 0; i < cart.length; i++);
     cart.shift();
-    return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
+    return `Your total cost is $${CartTotal()}, which will be charged to the card ${cardNumber}.`;
     } else { 
       return 'Sorry, we don\'t have a credit card on file for you.';
 }
